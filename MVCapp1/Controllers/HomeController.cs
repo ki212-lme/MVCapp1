fd70038443f6067e5199ea0fb74eff73ec5c9597
Maks Limont <ki212_lme@student.ztu.edu.ua>
2022-04-02 17:32:10 +0300

    created repository for MVCapp1

---
+++ MVCapp1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCapp1.Models;
+using System.Diagnostics;
+
+namespace MVCapp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Hello() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Hello(Person person) {
+ 
+            return View("HelloName",person);
+        }
+    }
+}
---
+++ MVCapp1/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace MVCapp1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
